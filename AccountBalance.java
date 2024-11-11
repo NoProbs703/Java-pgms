e88b92e7826d9ec723d01093e6cc0ce13bfe7f49
NoProbs703 <nishanths.cs23@bmsce.ac.in>
2024-11-11 15:42:38 +0530

    AccountBalance.java

--- AccountBalance.java
+++ AccountBalance.java
@@ -2,20 +2,21 @@ package mypack;
 
 class Balance {
     String name;
-    float balance;
+    double balance;
 
-    Balance(String s, float f) {
+    Balance(String s, double f) {
         name = s;
         balance = f;
     }
 
     void displayshow() {
         if (balance < 0) {
-            System.out.println("No Balance!");
-        }
-        if (balance > 0) {
+            System.out.println("Account " + name + " has insufficient balance!");
+        } else if (balance > 0) {
             System.out.println("Name: " + name);
             System.out.println("Balance: " + balance);
+        } else {
+            System.out.println("Account " + name + " has a zero balance.");
         }
     }
 }
@@ -23,9 +24,9 @@ class Balance {
 class AccountBalance {
     public static void main(String args[]) {
         Balance current[] = new Balance[3];
-        current[0] = new Balance("A", 1000.0f);
-        current[1] = new Balance("B", 2000.0f);
-        current[2] = new Balance("C", -2.0f);
+        current[0] = new Balance("A", 1000.0);  
+        current[1] = new Balance("B", 2000.0);  
+        current[2] = new Balance("C", -2.0);    
 
         for (int i = 0; i < 3; i++) {
             current[i].displayshow();
